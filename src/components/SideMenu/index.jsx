0038efed2e296a16810dd77451dbea9b48681e4c
Rafael Loren175 <rafael.loren175@gmail.com>
2024-02-01 13:59:56 -0300

    add sidemenu structure

---
+++ src/components/SideMenu/index.jsx
@@ -0,0 +1,13 @@
+import { Container } from "./styles"
+import { Input } from "../Input"
+
+export function SideMenu() {
+  return (
+    <Container>
+      x icon
+      <h4>Menu</h4>
+      <Input placeholder="Busque por pratos ou ingredientes" />
+      <h4>Sair</h4>
+    </Container>
+  )
+}
