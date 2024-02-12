39c3446f72e5fd543be385d8880dc77183b56d9c
Rafael Loren175 <rafael.loren175@gmail.com>
2024-02-12 14:43:38 -0300

    add counter logic to amount component

--- src/components/Amount/index.jsx
+++ src/components/Amount/index.jsx
@@ -1,13 +1,27 @@
 import { Container } from "./styles"
 
+import { useState } from "react"
+
 import { PiPlus, PiMinus } from "react-icons/pi"
 
-export function Amount({text}) {
+export function Amount() {
+  const [counter, setCounter] = useState(1)
+
+  const increaseCounter = () => {
+    setCounter(counter + 1)
+  }
+
+  const decreaseCounter = () => {
+    if (counter > 1) {
+      setCounter(counter - 1)
+    }
+  }
+
   return (
     <Container>
-      <PiMinus/>
-      <p>{text}</p>
-      <PiPlus />
+      <PiMinus onClick={decreaseCounter} />
+      <p>{counter}</p>
+      <PiPlus onClick={increaseCounter} />
     </Container>
   )
 }
