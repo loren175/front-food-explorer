dee75c8263a93a5bef77eb8dcac70f1dd1d18a6a
Rafael Loren175 <rafael.loren175@gmail.com>
2024-02-20 09:33:03 -0300

    validate user

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,10 +2,20 @@ import { BrowserRouter } from "react-router-dom"
 import { AppRoutes } from "./app.routes"
 import { AuthRoutes } from "./auth.routes"
 import { useAuth } from "../hooks/auth"
+import { useEffect } from "react"
+import { api } from "../services/api"
 
 export function Routes() {
-  const { user } = useAuth()
+  const { user, signOut } = useAuth()
   const isAdmin = user ? user.is_admin : false
+
+  useEffect(() => {
+    api.get("/users/validated").catch((error) => {
+      if (error.response?.status === 401) {
+        signOut()
+      }
+    })
+  }, [])
 
   return (
     <BrowserRouter>
