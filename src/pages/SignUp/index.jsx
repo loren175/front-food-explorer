e2406f259867fdafddb76c446f7da8b52d0ba46d
Rafael Loren175 <rafael.loren175@gmail.com>
2024-02-21 09:54:38 -0300

    add minimum carac to password

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -27,6 +27,9 @@ export function SignUp() {
     if (!name || !email || !password) {
       return alert("Preencha todos os campos!")
     }
+    if (password.length < 6) {
+      return alert("A senha deve ter no mínimo 6 caracteres!")
+    }
 
     api
       .post("/users", { name, email, password })
