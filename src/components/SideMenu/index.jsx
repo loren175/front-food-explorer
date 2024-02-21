7573b7cacdea55f14869aafc93b2f9d316614899
Rafael Loren175 <rafael.loren175@gmail.com>
2024-02-21 08:44:32 -0300

    bug fix

--- src/components/SideMenu/index.jsx
+++ src/components/SideMenu/index.jsx
@@ -40,11 +40,11 @@ export function SideMenu({ setSearch, isAdmin, menuIsOpen, onCloseMenu, isDisabl
           $setsearch={setSearch}
           placeholder="Busque por pratos ou ingredientes"
         />
-        {isAdmin && (
+        {isAdmin ? (
           <div className="btn-border">
             <ButtonText text="Novo Prato" onClick={handleNewDishClick} />
           </div>
-        )}
+        ) : ( <></>)}
         <div className="btn-border">
           <ButtonText onClick={handleSignOutClick} text="Sair" />
         </div>
