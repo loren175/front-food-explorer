775761fb58e7416650d85f6a71ac9931a9ab9cdb
Rafael Loren175 <rafael.loren175@gmail.com>
2024-02-22 08:07:38 -0300

    change baseURL

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,8 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: "http://localhost:3333",
+  baseURL: "https://api-food-explorer-njhs.onrender.com",
   withCredentials: true,
 })
 
 // Para uso local mude para baseURL: "http://localhost:3333"
-// https://api-food-explorer-njhs.onrender.com
